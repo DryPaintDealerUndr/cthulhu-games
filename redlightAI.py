84014155677d3897b68e454b3a6348e0ec230b15
DryPaintDealerUndr <101069292+DryPaintDealerUndr@users.noreply.github.com>
2022-03-24 09:59:05 +0000

    Add files via upload

---
+++ redlightAI.py
@@ -0,0 +1,11 @@
+def basicAI():
+    #makes more mistakes and reacts slower
+def mediumAI():
+    #makes less mistakes and reacts quicker
+def advancedAI():
+    #makes few mistakes and reacts slightly quicker
+def ultimateAI():
+    #basically makes no mistakes ever and same reaction time as advancedAI
+def oldmanAI():
+    #Chad AI
+    #cannot make a mistake but takes a while to finish
