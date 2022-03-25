28c3f3d2322a760738cf018dcc61ee8a7a4ab7b3
DryPaintDealerUndr <101069292+DryPaintDealerUndr@users.noreply.github.com>
2022-03-25 09:52:47 +0000

    Added info about each AI

--- redlightAI.py
+++ redlightAI.py
@@ -1,11 +1,15 @@
 def basicAI():
     #makes more mistakes and reacts slower
+    #1/10 chance of mistake, reacts ~~10-20ticks slow (even posible?)
 def mediumAI():
     #makes less mistakes and reacts quicker
+    #1/15 chance of mistake, reacts ~~7-15ticks slow (even posible?)
 def advancedAI():
     #makes few mistakes and reacts slightly quicker
+    #1/20 chance of mistake, reacts ~~4-10ticks slow
 def ultimateAI():
     #basically makes no mistakes ever and same reaction time as advancedAI
+    #1/100 chance of mistake, reacts ~~4-7ticks slow
 def oldmanAI():
     #Chad AI
     #cannot make a mistake but takes a while to finish
